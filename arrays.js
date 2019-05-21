382ddf66a95bff153c52feceff759fe5c9c90dbf
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-21 23:40:03 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+function addElementToBeginningOfArray (array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray (array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element);
+  return array;
+}
