280a9ee5760f892af57e2cadfd5c3fea648e31b1
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-21 23:50:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  
+  var newArray = array.slice(0, array.length - 1);
+  return newArray;
 }
